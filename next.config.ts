b8420504a3db49bb453e079cef7fce96925939db
Raxmonov Ruslan <119409762+ruslan-raxmonov@users.noreply.github.com>
2026-09-24 15:08:02 +0500

    Add next.config.ts

---
+++ next.config.ts
@@ -0,0 +1,16 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  reactStrictMode: true,
+  images: {
+    dangerouslyAllowSVG: true,
+    contentDispositionType: "attachment",
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+    remotePatterns: [
+      { protocol: "https", hostname: "**" },
+      { protocol: "http", hostname: "localhost" },
+    ],
+  },
+};
+
+export default nextConfig;
